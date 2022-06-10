7f6399792b8290cff6a6302f40d042967d45c29c
Oege Hiddema <oegehiddema@gmail.com>
2022-06-10 10:36:02 +0200

    Extra line added in App.class for my_first_branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Line added for my_first_branch");
     }
 }
